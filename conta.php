66c7f247e3f0800ec24d6a7ed93f6390c319ceb0
php 12 <php.12@jerry.cec.ime.usp.br>
2018-01-29 20:08:58 -0200

    classe de metodos

--- conta.php
+++ conta.php
@@ -8,13 +8,19 @@ class conta
   public static $contador=0; //atributo da classe
 
 
-  private function __construct() // metodo privado 
+  private function __construct() // metodo privado
   {
    self::$contador++;
   }
 
   static function criaconta(){// metodo da classe e não do objeto uso do static
     return new conta();
+  }
+
+  static function zeracontador(){
+    echo "contador:" . conta::$contador . PHP_EOL;
+    self::$contador=0;
+    echo "contador:" . conta::$contador . PHP_EOL;
   }
 }
 
--- testa_conta.php
+++ testa_conta.php
@@ -2,10 +2,19 @@
 
 require 'conta.php';
 
+echo "contador:" . conta::$contador . PHP_EOL;// acesso ao atributo da classe deve chamar o metodo pela classe::$atributo da classe
+
 $conta1= conta::criaconta();
+$conta1->numero=12542;
+echo "contador:" . conta::$contador . PHP_EOL;
+echo "Conta:$conta1->numero" . PHP_EOL;
 
 $conta2=conta::criaconta();
-
+$conta2->numero=12544;
 echo "contador:" . conta::$contador . PHP_EOL;// acesso ao atributo da classe deve chamar o metodo pela classe::$atributo da classe
+echo "Conta:$conta2->numero" . PHP_EOL;
+
+ conta::zeracontador();
+
 
  ?>
